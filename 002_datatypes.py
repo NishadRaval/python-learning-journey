ccb4cf796d1e8b3596549cf9334697a1e9fc1d8a
Nishad Raval <nishadsraval@gmail.com>
2026-07-14 21:19:45 +0530

    Day-1: Added Datatype examples.

---
+++ 002_datatypes.py
@@ -0,0 +1,14 @@
+name = "Nishad"      # String
+age = 21             # Integer
+height = 5.8         # Float
+is_student = True    # Boolean
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
