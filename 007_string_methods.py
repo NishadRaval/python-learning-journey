5cd062f243df88835cc6734b70c219ebe3fe1c4f
Nishad Raval <nishadsraval@gmail.com>
2026-07-14 21:45:53 +0530

    Day-1: Added String-Methods examples.

---
+++ 007_string_methods.py
@@ -0,0 +1,15 @@
+name = "nishad raval"
+
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+
+print(name.replace("nishad", "python"))
+
+print(name.find("raval"))
+
+print(name.count("a"))
+
+text = "   Hello World   "
+print(text.strip())
