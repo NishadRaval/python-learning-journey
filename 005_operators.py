34395bca5aebb63272dbc157f434179f437ada18
Nishad Raval <nishadsraval@gmail.com>
2026-07-14 21:39:06 +0530

    Day-1: Added Operators examples.

---
+++ 005_operators.py
@@ -0,0 +1,43 @@
+# Arithmetic Operators
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+
+# Comparison Operators
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+# Logical Operators
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+
+
+# Assignment Operators
+num = 5
+
+num += 2
+print(num)
+
+num -= 1
+print(num)
+
+num *= 2
+print(num)
+
+num /= 3
+print(num)
