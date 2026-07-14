c0887cf006a76243af2937e803591be1514a78e4
Nishad Raval <nishadsraval@gmail.com>
2026-07-14 21:43:04 +0530

    Day-1: Added String examples.

---
+++ 006_strings.py
@@ -0,0 +1,21 @@
+# Creating Strings
+name = "Nishad"
+print(name)
+
+# Indexing
+print(name[0])   # N
+print(name[1])   # i
+print(name[2])   # s
+
+# Negative Indexing
+print(name[-1])  # d
+print(name[-2])  # a
+
+# Slicing
+print(name[0:3])   # Nis
+print(name[2:5])   # sha
+print(name[:4])    # Nish
+print(name[3:])    # had
+
+# Length of String
+print(len(name))    #6  
