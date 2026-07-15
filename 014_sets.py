d933b0efdb7db5379ecfbe66544958282cef19bd
Nishad Raval <nishadsraval@gmail.com>
2026-07-15 08:44:29 +0530

    Day-2: Added Sets and its methods examples.

---
+++ 014_sets.py
@@ -0,0 +1,7 @@
+s = set() #empty set
+print(type(s)) #it will return the type of the variable s which is <class 'set'>
+
+a = {1, 2, 3, 4, 5}
+b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(a)
+print(b)
---
+++ 015_sets_methods.py
@@ -0,0 +1,9 @@
+a = {1, 2, 3, 4, 5}
+b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+print(a, a.add(6))
+print(a, a.remove(3))
+print(len(a))
+print(a, a.pop())
+print(a.union(b))
+print(a.intersection(b))
