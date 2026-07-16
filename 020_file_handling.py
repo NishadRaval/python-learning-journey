cbdfd15712b07ea4d9aa0ce6f05c3bd4ea69d1f8
Nishad Raval <nishadsraval@gmail.com>
2026-07-16 11:52:47 +0530

    Day-3: Added File Handling examples.

---
+++ 020_file_handling.py
@@ -0,0 +1,15 @@
+import random
+def game():
+    score = random.randint(1,61)
+    with open("score.txt") as f:
+        hiscore = f.read()
+        if hiscore != "":
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0
+    print(f"Your score {score}")
+    if score > hiscore:
+        with open("score.txt","w") as f:
+            f.write(str(score))
+    return score
+game()
